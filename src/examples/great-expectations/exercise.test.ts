e65d989388900be0084abab7e0b351365e5859d7
xaraf <nu01280@gmail.com>
2023-07-16 15:20:22 +0100

    chaching

--- src/examples/great-expectations/exercise.test.ts
+++ src/examples/great-expectations/exercise.test.ts
@@ -15,7 +15,7 @@ import { KanbanBoard } from '$lib/kanban-board';
 it(
   'should pass if the two numbers would add up correctly in a language other than JavaScript',
   () => {
-    expect(0.2 + 0.1).toEqual(0.3);
+    expect(0.2 + 0.1).toBeCloseTo(0.3);
   },
 );
 
